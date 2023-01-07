0f8cc51389f8ed294e0a668231879280c8db884d
tsunoda <tsunoda@aomori-u.ac.jp>
2023-01-07 15:45:01 +0900

    StreamBuilderを使う形で簡素化、ボタンでデータ変更

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,6 @@ void main() async {
   runApp(MyApp());
 }
 
-// final dummySnapshot = [
-//   {"name": "Filip", "votes": 15},
-//   {"name": "Abraham", "votes": 14},
-//   {"name": "Richard", "votes": 11},
-//   {"name": "Ike", "votes": 10},
-//   {"name": "Justin", "votes": 1},
-// ];
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -37,75 +29,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: _buildBody(context),
+        appBar: AppBar(title: Text('Baby Name Votes')),
+        body: buildBody(context)
     );
   }
 
-  Widget _buildBody(BuildContext context) {
-    // return _buildList(context, dummySnapshot);
+  Widget buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('baby').snapshots(), // babyは各々のコレクションIDに変更してください
+      stream: FirebaseFirestore.instance.collection('baby').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        return _buildList(context, snapshot.data!.docs);
+        print("##################################################### Firestore Access start");
+        snapshot.data!.docs.forEach((elem) {
+          print(elem.get('name'));
+          print(elem.get('votes'));
+        });
+        print("##################################################### Firestore Access end");
+        return Column(
+          children: [
+            Expanded(
+                child: Text("てすと")
+            ),
+            OutlinedButton(
+              onPressed: () {
+                snapshot.data!.docs[0].reference.update({'votes': FieldValue.increment(1)});
+                },
+              child: Text('ぼたん'),
+            ),
+          ],
+        );
       },
     );
   }
-
-  // Widget _buildList(BuildContext context, List<Map> snapshot) {
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  // Widget _buildListItem(BuildContext context, Map data) {
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    // final record = Record.fromMap(data as Map<String, dynamic>);
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          // onTap: () => print(record),
-          onTap: () => record.reference.update({'votes': FieldValue.increment(1)}),
-        ),
-      ),
-    );
-  }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  // Record.fromMap(Map<String, dynamic> map)
-  //     : assert(map['name'] != null),
-  //       assert(map['votes'] != null),
-  //       name = map['name'],
-  //       votes = map['votes'];
-
-  Record.fromMap(Map<String, dynamic> map, {required this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data() as Map<String, dynamic>, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$votes>";
 }
