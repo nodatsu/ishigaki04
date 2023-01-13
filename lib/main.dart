67e3971f1b540b866e4f9ef2faac6070d7e87f3a
tsunoda <tsunoda@aomori-u.ac.jp>
2023-01-13 23:17:24 +0900

    OAuth認証でgoogle-services.jsonを使わないように修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' hide Colors;
 
 void main() async {
+  FirebaseOptions options = FirebaseOptions(
+      apiKey: 'AIzaSyC9x3iG5eK3fzlkkS0DGhWD0eH5b5w2GfM',
+      appId: '1:887390408274:android:ffdfe22deac69a3de1598b',
+      messagingSenderId: '887390408274',
+      projectId: 'calendarishigaki');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: options);
   runApp(MyApp());
 }
 
@@ -30,11 +39,18 @@ class _MyHomePageState extends State<MyHomePage> {
   late CollectionReference cref;
   late AppointmentDataSource dataSource;
 
+  final GoogleSignIn googleSignIn = GoogleSignIn(
+    scopes: <String>[CalendarApi.calendarScope],
+  );
+
   @override
   void initState() {
     super.initState();
     cref = FirebaseFirestore.instance.collection('schedule');
     dataSource = getCalendarDataSource();
+
+    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {});
+    googleSignIn.signInSilently();
   }
 
   @override
@@ -49,14 +65,14 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        print("##################################################### Firestore Access start");
+        print("########## Firestore Access start");
         snapshot.data!.docs.forEach((elem) {
           print(elem.get('email'));
           print(elem.get('subject'));
           print(elem.get('start_time').toDate().toLocal().toString());
           print(elem.get('end_time').toDate().toLocal().toString());
         });
-        print("##################################################### Firestore Access end");
+        print("########## Firestore Access end");
 
         dataSource.appointments!.clear(); // 現在の予定を一旦すべて消去
         snapshot.data!.docs.forEach((elem) {
@@ -69,7 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
             endTimeZone: '',
           ));
         });
-        dataSource.notifyListeners(CalendarDataSourceAction.reset, dataSource.appointments!); // カレンダの再描画
+        dataSource.notifyListeners(CalendarDataSourceAction.reset,
+            dataSource.appointments!); // カレンダの再描画
 
         return Column(
           children: [
@@ -81,12 +98,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             OutlinedButton(
               onPressed: () {
-                cref.add({
-                  'email': 'test03@gmail.com',
-                  'subject': '予定3',
-                  'start_time': DateTime.now().add(Duration(hours: 1)),
-                  'end_time': DateTime.now().add(Duration(hours: 3)),
-                });
+                // cref.add({
+                //   'email': 'test03@gmail.com',
+                //   'subject': '予定3',
+                //   'start_time': DateTime.now().add(Duration(hours: 1)),
+                //   'end_time': DateTime.now().add(Duration(hours: 3)),
+                // });
+                getGoogleEventsData();
               },
               child: Text('ぼたん'),
             ),
@@ -101,10 +119,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return AppointmentDataSource(appointments);
   }
+
+  Future<List<Event>> getGoogleEventsData() async {
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    final GoogleAPIClient httpClient =
+        GoogleAPIClient(await googleUser!.authHeaders);
+    final CalendarApi calendarApi = CalendarApi(httpClient);
+    final Events calEvents = await calendarApi.events.list(
+      "primary",
+    );
+    final List<Event> appointments = <Event>[];
+    if (calEvents.items != null) {
+      for (int i = 0; i < calEvents.items!.length; i++) {
+        final Event event = calEvents.items![i];
+        if (event.start == null) {
+          continue;
+        }
+        print('########## email: ' + (googleUser.email).toString());
+        print('########## start-time: ' +
+            (event.start!.date ?? event.start!.dateTime!.toLocal()).toString());
+        print('########## end-time: ' +
+            (event.end!.date ?? event.end!.dateTime!.toLocal()).toString());
+        print('########## subject: ' + (event.summary).toString());
+        appointments.add(event);
+      }
+    }
+    return appointments;
+  }
 }
 
 class AppointmentDataSource extends CalendarDataSource {
   AppointmentDataSource(List<Appointment> source) {
     appointments = source;
   }
+}
+
+class GoogleAPIClient extends IOClient {
+  final Map<String, String> _headers;
+
+  GoogleAPIClient(this._headers) : super();
+
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+      super.head(url,
+          headers: (headers != null ? (headers..addAll(_headers)) : headers));
 }
